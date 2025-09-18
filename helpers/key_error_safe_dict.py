31b8d099877a7153edc01b86f57c1850c186ab3e
MKinanA <mkinanahsan@gmail.com>
2025-09-18 17:22:52 +0700

    Updated the `KeyErrorSafeDict` helper so it accepts an object argument for what to return on missing keys

--- helpers/key_error_safe_dict.py
+++ helpers/key_error_safe_dict.py
@@ -1,2 +1,6 @@
 class KeyErrorSafeDict(dict):
-    def __missing__(self, key) -> None: return None
+    to_return_on_missing_key = None
+    def __init__(self, value, to_return_on_missing_key: object = None):
+        self.to_return_on_missing_key = (lambda self, key: to_return_on_missing_key(key)) if callable(to_return_on_missing_key) else to_return_on_missing_key
+        super().__init__(value)
+    def __missing__(self, key): return self.to_return_on_missing_key(self, key) if callable(self.to_return_on_missing_key) else self.to_return_on_missing_key
