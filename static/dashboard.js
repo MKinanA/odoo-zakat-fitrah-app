bd25dc259fdd0d7db2ba96e2912c2ca5c7448bb9
MKinanA <mkinanahsan@gmail.com>
2025-09-18 23:37:56 +0700

    Fixed the code

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,4 +1,4 @@
-setInterval(async () => {
-    const data = JSON.parse((await fetch('{API_ROUTE_PREFIX}/recap')).text())
-    for (key in data) document.querySelectorAll(`[data-key="${key}"]`).forEach(element => element.innerHTML = data[key])
-}, 10000);
+setInterval(async () => {{
+    const data = JSON.parse(await (await fetch('{API_ROUTE_PREFIX}/recap')).text());
+    for (key in data) document.querySelectorAll(`[data-key="${{key}}"]`).forEach(element => element.innerHTML = data[key]);
+}}, 10000);
